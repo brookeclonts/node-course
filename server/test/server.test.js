2f61d8e9125bcfaf2e4861f8dd010086d6d158e1
Brooke Clonts <brooke.clonts@ariix.com>
2017-01-23 16:32:46 -0700

    added tests for get todos

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -5,14 +5,38 @@ const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 const {User} = require('./../models/user');
 
-// clears db so assumptions made below are accurate
-beforeEach((done) => {
-    Todo.remove({}).then(() => done());
-});
+const todos = [{
+    text: 'first test todo'
+}, {
+    text: 'second test todo'
+}];
 
 beforeEach((done) => {
-    User.remove({}).then(() => done());
+    Todo.remove({}).then(() => {
+        Todo.insertMany(todos);
+    }).then(() => done());
 });
+
+// const users = [{
+//     email: 'email1@email.com'
+// }, {
+//     email: 'email2@email.com'
+// }];
+
+// beforeEach((done) => {
+//     User.remove({}).then(() => {
+//         User.insertMany(users);
+//     }).then(() => done());
+// });
+
+// clears db so assumptions made below are accurate
+// beforeEach((done) => {
+//     Todo.remove({}).then(() => done());
+// });
+
+// beforeEach((done) => {
+//     User.remove({}).then(() => done());
+// });
 
 describe('POST /todos', () => {
     it('should create a new todo', (done) => {
@@ -30,7 +54,7 @@ describe('POST /todos', () => {
                     return done(err);
                 }
 
-                Todo.find().then((todos) => {
+                Todo.find({text}).then((todos) => {
                     expect(todos.length).toBe(1);
                     expect(todos[0].text).toBe(text);
                     done();
@@ -39,27 +63,39 @@ describe('POST /todos', () => {
     });
 });
 
-describe('POST /users', () => {
-    it('should create a new user', (done) => {
-        var email = 'email@email.com';
-
+describe('GET todos', () => {
+    it('should get all todos', (done) => {
         request(app)
-            .post('/users')
-            .send({email})
+            .get('/todos')
             .expect(200)
-            .expect((res)=> {
-                expect(res.body.email).toBe(email);
+            .expect((res) => {
+                expect(res.body.todos.length).toBe(2);
             })
-            .end((err, res) => {
-                if (err) {
-                    return done(err);
-                }
-
-                User.find().then((users) => {
-                    expect(users.length).toBe(1);
-                    expect(users[0].email).toBe(email);
-                    done();
-                }).catch((e) => done(e));
-            });
+            .end(done);
     });
-});
+})
+
+// describe('POST /users', () => {
+//     it('should create a new user', (done) => {
+//         var email = 'email@email.com';
+
+//         request(app)
+//             .post('/users')
+//             .send({email})
+//             .expect(200)
+//             .expect((res) => {
+//                 expect(res.body.email).toBe(email);
+//             })
+//             .end((err, res) => {
+//                 if (err) {
+//                     return done(err);
+//                 }
+
+//                 User.find().then((users) => {
+//                     expect(users.length).toBe(3);
+//                     expect(users[0].email).toBe(email);
+//                     done();
+//                 }).catch((e) => done(e));
+//             });
+//     });
+// });
